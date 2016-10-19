6a3a61c990ff9d7f4abea2b57f76864774ee5fd5
melkorchained <priyanksmehta@gmail.com>
2016-10-19 16:14:10 -0700

    Added comments, docstring

--- territories.py
+++ territories.py
@@ -1,5 +1,24 @@
-class Territory(object):
 
+class Territory(object):
+	'''
+	Class describing a territory. Each territory can possess either Power, Supply, both, or neither
+	In addition, a territory can possess either a castle or a stronghold, and a port
+	A territory has a garrison if:
+	a) It is a House's starting capital
+	b) It is a specially reinforced territory (eg: King's Landing, The Eyrie)
+	
+	A territory loses its garrison if it is defeated, and doesn't regain it for the remainder of the game
+
+	The following externally usable methods will be contained in this class:
+
+	current_strength: Returns the combat strength of the territory
+
+	current_units: Returns all units in the territory
+
+	supporting_units: returns all units supporting this territory
+
+	order_token: returns the order token placed on the territory
+	'''
 	def __init__(self, castle, stronghold, power, supply, port, garrison):
 		self.castle = castle
 		self.stronghold = stronghold
@@ -9,17 +28,20 @@ class Territory(object):
 		self.garrison = garrison
 		self.adjacent_sea = []
 		self.adjacent_land = []
-		self.land_by_sea = []
-		if self.castle and self.stronghold:
-			raise "Invalid entry, a territory cannot possess both a castle and a stronghold"
 
-	def current_strength(self):
+	def current_strength(self, combat_status, order):
 		pass
 
 	def current_units(self):
 		pass
 
-	def mustering():
+	def supporting_units(self):
+		pass
+
+	def order_token(self):
+		pass
+
+	def mustering_strength():
 		if self.castle:
 			return 1
 		elif self.stronghold:
