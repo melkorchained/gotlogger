8e9169633eafbf6dc83cdb95830b811a1ac7689b
melkorchained <priyanksmehta@gmail.com>
2016-10-20 13:38:40 -0700

    main gameplay

---
+++ game.py
@@ -0,0 +1,7 @@
+import territories
+import board
+
+if __name__ == '__main__':
+	# Initialize all territories
+	winterfell = territories.Territory() #add parameters here
+	martell = houses.Martell() # create houses file, House class and inheriting individual house classes
